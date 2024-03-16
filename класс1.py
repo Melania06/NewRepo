8b9641a0d16987eeb6751e7122a699a410b53288
Melania06 <118605668+Melania06@users.noreply.github.com>
2024-03-16 10:46:24 +0300

    Add files via upload

---
+++ класс1.py
@@ -0,0 +1,16 @@
+import csv
+def readFile(namefile):
+    device = []
+    with open(namefile,'r') in namefile:
+       
+        devices.append({'Company': device['Company'], 'Product': device['Product'], 'TypeName': device['TypeName'], 'Inches': device['Inches'], 'Cpu': device['Cpu'], 'Ram': device['Ram'],'Memory': device['Memory'], 'Gpu': device['Gpu'], 'Price': device['Price']})
+    return devices
+
+
+    
+
+
+
+
+
+
