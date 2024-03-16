a134e0726b6ea16d800e0455acb16af19cfd6264
Melania06 <118605668+Melania06@users.noreply.github.com>
2024-03-16 10:52:31 +0300

    Add files via upload

--- класс1.py
+++ класс1.py
@@ -1,10 +1,11 @@
 import csv
 def readFile(namefile):
     device = []
-    with open(namefile,'r') in namefile:
-       
-        devices.append({'Company': device['Company'], 'Product': device['Product'], 'TypeName': device['TypeName'], 'Inches': device['Inches'], 'Cpu': device['Cpu'], 'Ram': device['Ram'],'Memory': device['Memory'], 'Gpu': device['Gpu'], 'Price': device['Price']})
-    return devices
+    with open(namefile,'r') as file:
+     reader = csv.DictReader(file, delimiter='*')
+     for device in reader:
+        device.append({'Company': device['Company'], 'Product': device['Product'], 'TypeName': device['TypeName'], 'Inches': device['Inches'], 'Cpu': device['Cpu'], 'Ram': device['Ram'],'Memory': device['Memory'], 'Gpu': device['Gpu'], 'Price': device['Price']})
+    return device
 
 
     
